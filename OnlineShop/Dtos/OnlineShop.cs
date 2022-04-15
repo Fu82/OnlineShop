6a8b8146d4f66d1ea641b903bf2f171efcf9a822
I552\Administrator <aa>
2022-04-15 10:54:02 +0800

    添加项目文件。

---
+++ OnlineShop/Dtos/OnlineShop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Dtos
+{
+    public class OnlineShopSelectDto
+    {
+        public int Id { get; set; }
+        public string Acc { get; set; }
+        public string Pwd { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ShopGold { get; set; }
+        public byte Level { get; set; }
+        public byte Suspension { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
