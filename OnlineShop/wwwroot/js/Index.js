66330b92d9e9cf2114ff8e6cd7fbc9851f83b433
I552\Administrator <aa>
2022-05-20 18:27:31 +0800

    20220520 1.商品內頁更新 部分邏輯需修改

--- OnlineShop/wwwroot/js/Index.js
+++ OnlineShop/wwwroot/js/Index.js
@@ -1,4 +1,5 @@
 ﻿$(document).ready(function () {
+    //商品清單
     $.ajax({
         type: "GET",
         url: "/api/Product/GetProduct",
@@ -10,21 +11,16 @@
             var rows = "";
             /*var imgTag = ""*/
             for (var i in data) {
-                //判断图片是否存在(還須修正)
-                //if (data[i].f_img === "") {
-                //    imgTag = "<a class='prodImg'>" + "無圖片" + "</a>"
-                //} else {
-                //    imgTag = "<a class='prodImg'>" + "<img src='" + data[i].f_img + "'/>" + "</a>"
-                //}
                 rows += "<div>" +
-                    /*imgTag +*/ "<a class='prodImg'>" + "<img id='join' src='" + data[i].f_img + "'/>" + "</a>" +
+                    "<a class='prodImg'>" + "<img id='join' src='" + data[i].f_img + "' onerror='nofind()' />" + "</a>" + 
                     "<div class='prodName'>" +
                     "<h5 class='prodNick'>" + "<a>" +
-                    "<span id='join' class='prodExtra'>" + data[i].f_name + "</span>" +
+                    "<span class='prodExtra'>" + data[i].f_name + "</span>" +
                     "</a>" + "</h5>" + "</div>" +
-                    "<div class='prodPrice'>" +
-                    "<span class='prodDollar'>" + "$" + "<span class='prodValue'>" + data[i].f_price + "</span>" + "</span>" +
+                    "<div class='prodPrice prodDollar'>" +
+                    "$" + "<span class='prodValue'>" + data[i].f_price + "</span>" +
                     "</div>" +
+                    "<div style='display:none'>" + data[i].f_content + "</div>" + //商品內容隱藏
                     "<div class='prodJoin'>" +
                     "<input type='Button' value='加入購物車' />" +
                     "<input type='Button' value='加入喜好' />" +
@@ -40,39 +36,50 @@
         }
     });
 
+    //組合商品內頁
     $("#prodBody").on('click', '#join', function () {
-        var currentRow1 = $(this).closest("div");
-        var currentRow2 = $(this).closest("a");
+        var ProdRow = $(this).closest("div");
 
-        var col1 = currentRow2.find("a:eq(0)").text();
-        var col2 = currentRow1.find("div:eq(0)").text();
-        var col3 = currentRow1.find("div:eq(1)").text();
+        var ProdImg = ProdRow.find("a > img").attr('src');
+        var ProdName = ProdRow.find("div:eq(0)").text();
+        var ProdPrice = ProdRow.find("div > span").text();
+        var ProdContent = ProdRow.find("div:eq(2)").text();
 
         if ($("#prodInside").css("display") == "none") {
             var InsideRow = "<div style='width: 100%;'>" +
-                "<div style='width: 35%;float: left;'>" + "<div>" + "<img style='width: 480px;' src='" + col1 +"'/>" + "</div>" + "</div>" +
-                "<div style='float: right;width: 55%;color: #666;height: 500px;margin: 50px;'>" +
-                "<div style='margin-bottom: 20px;font: bold 20px / 27px Arial, Verdana, 'Yu Gothic', 'MS Gothic', 'Microsoft JhengHei', Helvetica, sans - serif;'>" + "<h5>" +
-                "<span>" + col2 + "</span>" +
+                "<div style='width: 35%;float: left;'>" + "<div>" + "<img style='width: 400px;' src='" + ProdImg +"'/>" + "</div>" + "</div>" +
+                "<div style='float: right;width: 55%;color: #666;height: 400px;margin: 50px;'>" +
+                "<div style='margin-bottom: 20px;'>" + "<h5>" +
+                "<span>" + ProdName + "</span>" +
                 "</h5>" + "</div>" +
                 "<div>" +
-                "<span>" + "主機板晶片組：B460 記憶體：16GB DDR4 = 原廠8G + 創見8G(需自行安裝) 硬碟：512GB M.2 SSD 光碟機：DVD writer 8X 螢幕輸出介面：D - Sub(VGA) ．HDMI．DP 電源供應: 500W power supply(80 + Bronze, peak 550W) 其他：Wi - Fi 5(802.11ac) + Bluetooth 4.2(Dual band) 1 * 1 作業系統：Windows 11 家用版(64 bit) 保固：三年保固 / 到府收送" + "</span>" +
+                "<span>" + ProdContent + "</span>" +
                 "</div>" +
-                "<div>" +
-                "<span>" + col3 + "</span>" +
+                "<div>" + "$" +
+                "<span>" + ProdPrice + "</span>" +
                 "</div>" + "</div>" +
                 "</div>" +
                 "<div>" +
-                "<input type='Button' class='join' value='加入購物車' />" +
-                "<input type='Button' value='加入喜好' />" +
+                "<input type='Button' style='width:50%' value='加入購物車' />" +
+                "<input type='Button' style='width:50%' value='加入喜好' />" +
                 "</div>" +
-                "<input type = 'Button'  onclick = 'InsideCancel_Click()' value = '返回' /></div > ";
+                "<input type = 'Button'  onclick = 'InsideCancel_Click()' style='width:100%;color: #fff;background-color: #007bff;border-color: #007bff;margin-top: 30px;' value = '返回' />";
             $('#prodInsideBody').html(InsideRow);
             $("#prodInside").show();
             $("#prodList").addClass("ListSwitch");
             $("#prodSearch").addClass("ListSwitch");
         }
     });
+
+    $("#one").click(function () {
+        $("#two").toggle();
+    });
+
+    function nofind() {
+        var img = event.srcElement;
+        img.src = "images/product/noImg.png";
+        img.onerror = null;
+    }
 })
 
 //返回
@@ -81,23 +88,26 @@ function InsideCancel_Click() {
         $("#prodInside").hide();
         $("#prodList").removeClass("ListSwitch");
         $("#prodSearch").removeClass("ListSwitch");
+        location.reload();
     }
 }
 
 var ProdMenufun = {
+    //清單重組
     DrawProductList: function (prodArray) {
         var htmlText = "";
 
         for (var i = 0; i < prodArray.length; i++) {
             htmlText += "<div>" +
-                "<a class='prodImg'>" + "<img src='" + prodArray[i].f_img + "'/>" + "</a>" +
+                "<a class='prodImg'>" + "<img id='join' src='" + prodArray[i].f_img + "' onerror='nofind()' />" + "</a>" +
                 "<div class='prodName'>" +
                 "<h5 class='prodNick'>" + "<a>" +
                 "<span class='prodExtra'>" + prodArray[i].f_name + "</span>" +
                 "</a>" + "</h5>" + "</div>" +
-                "<div class='prodPrice'>" +
-                "<span class='prodDollar'>" + "$" + "<span class='prodValue'>" + prodArray[i].f_price + "</span>" + "</span>" +
+                "<div class='prodPrice prodDollar'>" +
+                "$" + "<span class='prodValue'>" + prodArray[i].f_price + "</span>" +
                 "</div>" +
+                "<div style='display:none'>" + prodArray[i].f_content + "</div>" + //商品內容隱藏
                 "<div class='prodJoin'>" +
                 "<input type='Button' value='加入購物車' />" +
                 "<input type='Button' value='加入喜好' />" +
@@ -149,20 +159,17 @@ var ProdMenufun = {
     //搜尋
     ProdListSearch: function () {
         var tempTable = $.extend(true, [], ProdJson);
-        var StrClassArr = "f_name"; //商品名稱為搜尋對象
         var searchvalue = $("#Search").val(); //輸入值
 
         if (searchvalue === "") {
-
             //組HTML,覆蓋
             ProdMenufun.DrawProductList(ProdJson);
 
         } else {
-
             //字串搜尋
-            var searchStr = function (Search) {
+            var searchvalue = function (Search) {
                 tempTable = tempTable.filter(function (item) { //filter搜尋json
-                    if (item[StrClassArr].indexOf(Search) >= 0) { //indexOf -> 有找到所鍵入文字則回傳 >=0
+                    if (item["f_name"].indexOf(Search) >= 0) { //indexOf -> 有找到所鍵入文字則回傳 >=0
                         return item //大於等於0則 return item
                     }
                 })
